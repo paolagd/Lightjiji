42b73d545d590fe06ee453f55a5704a24adb350f
J Heslop <jayrese.h@gmail.com>
2021-09-18 19:11:46 -0400

    add route templates; add db interface functions for Users table

---
+++ routes/product-router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router  = express.Router();
+const productQueries = require('./lib/products');
+
+// GET /products
+
+ 
+router.get("/", (req, res) => {
+
+  // db.query(`SELECT * FROM users;`)
+  //   .then(data => {
+  //     const users = data.rows;
+  //     res.json({ users });
+  //   })
+  //   .catch(err => {
+  //     res
+  //       .status(500)
+  //       .json({ error: err.message });
+  //   });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,12 +10,6 @@ const sass       = require("node-sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
 
-// PG database client/connection setup
-const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
-const db = new Pool(dbParams);
-db.connect();
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -33,13 +27,16 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const usersRoutes   = require("./routes/user-router"); 
+const productRoutes = require("./routes/product-router");  
+const messageRoutes = require("./routes/message-router");  
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/users", usersRoutes); 
+app.use("/api/products", productRoutes); 
+app.use("/api/messages", messageRoutes); 
+
 // Note: mount other resources here, using the same pattern above
 
 
