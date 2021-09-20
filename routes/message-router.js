0c4cc6d4db33b968303050118399e18c7a6132fb
J Heslop <jayrese.h@gmail.com>
2021-09-20 11:35:35 -0400

    Implement GET /api/messages/:other_id and POST /api/messages/:other_id endpoints.

--- routes/message-router.js
+++ routes/message-router.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const router  = express.Router();
 
-const { getAllUserConversations } = require('../lib/messages');
+const {
+  getAllUserConversations,
+  getMessagesWithUser,
+  sendMessage
+} = require('../lib/messages');
 
 // GET /api/messages
 router.get("/", (req, res) => {
-  const userId = 1; // sessions not setup yet - change me
-  getAllUserConversations(userId)
+  const myUserId = 1; // sessions not setup yet - change me
+  getAllUserConversations(myUserId)
     .then(conversations => {
       res.json(conversations);
+    })
+    .catch(errorMessage => res.status(500).json({ error: errorMessage }));
+});
+
+// GET /api/messages/:other_id
+router.get("/:other_id", (req, res) => {
+  const myUserId = 1;
+  const otherId = req.params.other_id;
+
+  getMessagesWithUser(myUserId, otherId)
+    .then(messages => {
+      res.json(messages)
+    })
+    .catch(errorMessage => res.status(500).json({ error: errorMessage }));
+});
+
+// POST /api/messages/:other_id
+router.post("/:other_id", (req, res) => {
+  const fromUserId = 1;
+  const toUserId = req.params.other_id;
+  const messageContent = req.body.message;
+
+  sendMessage(fromUserId, toUserId, messageContent)
+    .then(message => {
+      if (!message) {
+        throw "Message not sent!";
+      }
+
+      res.json(message);
     })
     .catch(errorMessage => res.status(500).json({ error: errorMessage }));
 });
