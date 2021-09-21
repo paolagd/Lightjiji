3e0d53984ca875585bdd521b222b39d830216690
Paola Garcia <pacri14@gmail.com>
2021-09-21 20:26:15 +0000

    Adds favorites template

--- routes/product-router.js
+++ routes/product-router.js
@@ -15,9 +15,9 @@ router.get('/', (req, res) => {
   Promise.all([productsPromise, categories])
     .then(result => {
       const templateVars = {
-        products : result[0],
-        categories : result[1],
-        user : userId
+        products: result[0],
+        categories: result[1],
+        user: userId
       };
 
       res.render('product-listings', templateVars);
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   const userId = req.session.user_id;
   if (!userId) {
-    res.error("The user doesn't exist;");
+    res.status(401).send('User not found')
     return;
   }
 
@@ -42,17 +42,45 @@ router.post('/', (req, res) => {
     .catch(errorMessage => res.status(500).json({ error: errorMessage }));
 });
 
+
+//Get favorites
+
+router.get('/favorites', (req, res) => {
+  const userId = req.session.user_id;
+
+  if (!userId) {
+    res.status(401).send('User not found')
+    return;
+  }
+
+  const filters = {
+    seller_id: userId,
+    id_featured: true
+  }
+
+  productQueries.getProductsSatisfying(filters)
+    .then(products => {
+      const templateVars = {
+        user: userId,
+        products,
+        timeago
+      }
+
+      res.render('favorites', templateVars);
+    })
+    .catch(errorMessage => {
+      res.status(500).json({ error: errorMessage });
+    });
+
+});
+
 // GET /products/:product_id
 router.get("/:product_id", (req, res) => {
   const userId = req.session.userId;
-  // if (!userId) {
-  //   res.error("The user doesn't exist;");
-  //   return;
-  // }
 
   productQueries.getProductById(req.params.product_id)
     .then(product => {
-      const templateVars = {user: userId, product, timeago}
+      const templateVars = { user: userId, product, timeago }
       res.render('review-products', templateVars);
     })
     .catch(errorMessage => {
@@ -103,6 +131,8 @@ router.delete("/:product_id", (req, res) => {
     });
 
 });
+
+
 
 
 module.exports = router;
