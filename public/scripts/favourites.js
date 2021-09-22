314685ab636b73e6998857a3e868dcbc32e2b45e
Paola Garcia <pacri14@gmail.com>
2021-09-22 23:59:07 +0000

    Corrects favorite buttons behaviour

---
+++ public/scripts/favourites.js
@@ -0,0 +1,58 @@
+$(() => {
+
+  window.addEventListener( "pageshow", function ( event ) {
+    if(performance.navigation.type == 2){
+      location.reload(true);
+     }
+  });
+
+  //Unfavorite event called from Favorites page
+  $('.unfavorite-button-favs').on('click', function(e) {
+    e.preventDefault();
+    const button = $(this)[0];
+    const productId = $(button).data("index");
+
+    $.ajax({
+        method: "PUT",
+        url:  `/api/products/${productId}/unfavourite`
+      }).done(() => {
+        $(button).closest('.product').slideUp();
+      });
+  });
+
+
+  //Unfavorite event called from Listings page page
+  $('.unfavorite-button').on('click', function(e) {
+    e.preventDefault();
+    const unfavoriteButton = $(this)[0];
+    const nextFavoriteButton = $(unfavoriteButton).parent().find('.favorite-button');
+    const productId = $(unfavoriteButton).data("index");
+
+    $.ajax({
+        method: "PUT",
+        url:  `/api/products/${productId}/unfavourite`
+      }).done(() => {
+        $(unfavoriteButton).toggle();
+        $(nextFavoriteButton).removeAttr('hidden');
+        $(nextFavoriteButton).show();
+      });
+  });
+
+  //Favorite event called from Listings page page
+  $('.favorite-button').on('click', function(e) {
+    e.preventDefault();
+    const favoriteButton = $(this)[0];
+    const nextUnfavoriteButton = $(favoriteButton).parent().find('.unfavorite-button');
+    const productId = $(favoriteButton).data("index");
+
+    $.ajax({
+        method: "PUT",
+        url:  `/api/products/${productId}/favourite`
+      }).done(() => {
+        $(favoriteButton).toggle();
+        $(nextUnfavoriteButton).removeAttr('hidden');
+        $(nextUnfavoriteButton).show();
+      });
+  });
+
+});
