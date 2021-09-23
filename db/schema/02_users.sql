a54980efaa777b7978ac590f678b580f924904d8
J Heslop <jayrese.h@gmail.com>
2021-09-23 14:57:54 -0400

    profile_pic column added to Users table

--- db/schema/02_users.sql
+++ db/schema/02_users.sql
@@ -5,5 +5,6 @@ CREATE TABLE Users (
   last_name VARCHAR(255) NOT NULL,
   phone_number VARCHAR(64),
   password VARCHAR(255) NOT NULL,
-  email VARCHAR(64) NOT NULL
+  email VARCHAR(64) NOT NULL,
+  profile_pic_url VARCHAR(255) NOT NULL
 );
--- db/seeds/02_users.sql
+++ db/seeds/02_users.sql
@@ -1,6 +1,6 @@
-INSERT INTO Users (first_name, last_name, phone_number, password, email)
-  VALUES ('Paola','G', '6472322222', 'password', 'paola@gmail.com'),
-  ('Harjeet','K', '647233332', 'password', 'harjeet@gmail.com'),
-  ('Jayrese','H', '6472325522', 'password', 'jay@gmail.com'),
-  ('Bob','T', '4152322222', 'password', 'bob@gmail.com'),
-  ('John','D','4165536158', 'password', 'john.doe@example.com');
+INSERT INTO Users (first_name, last_name, phone_number, password, email, profile_pic_url)
+  VALUES ('Paola','G', '6472322222', 'password', 'paola@gmail.com', 'https://i.imgur.com/nlhLi3I.png'),
+  ('Harjeet','K', '647233332', 'password', 'harjeet@gmail.com', 'https://i.imgur.com/z5LNkkB.png'),
+  ('Jayrese','H', '6472325522', 'password', 'jay@gmail.com', 'https://i.imgur.com/73hZDYK.png'),
+  ('Bob','T', '4152322222', 'password', 'bob@gmail.com', 'https://i.imgur.com/5fUVPRP.png'),
+  ('John','D','4165536158', 'password', 'john.doe@example.com', 'https://i.imgur.com/DVpDmdR.png');
